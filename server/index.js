d5594cab24b182adeb301a232944c80ce4c47006
webdevloper001 <princeofmj001@gmail.com>
2023-03-15 22:05:20 +0530

    my comment

--- server/index.js
+++ server/index.js
@@ -1,17 +1,16 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 import userRoutes from './routes/users.js';
 import questionsRoutes from './routes/Questions.js';
 import answerRoutes from './routes/Answers.js';
 
-dotenv.config();
-
 const app = express();
-app.use(express.json({limit: "30mb", extended: true}));
-app.use(express.urlencoded({limit: "30mb", extended: true}));
+dotenv.config();
+app.use(express.json({limit: "30mb", extended: true}))
+app.use(express.urlencoded({limit: "30mb", extended: "true"}))
 app.use(cors());
 
 app.get('/',(req, res) => {
@@ -24,8 +23,7 @@ app.use('/answer', answerRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-const CONNECTION_URL = process.env.MONGO_URI;
-
-mongoose.connect( CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => {console.log(`server running on port ${PORT}`); }))
-    .catch((err) => console.log(err.message));
+const DATABASE_URL = process.env.CONNECTION_URL
+mongoose.connect( DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => {console.log(`server running on PORT ${PORT}`) }))
+    .catch((err) => console.log(err.message))
